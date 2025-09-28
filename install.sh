5b28b588af225b866f6bf29570bf0bd26ea11d08
erwan <erwan.lemattre@hotmail.com>
2025-09-28 20:58:54 +0200

    chore: update eslint to version 9.36.0 in devDependencies

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ DEST_FOLDER=~/.local/share/gnome-shell/extensions/grimble\@lmt.github.io/
 glib-compile-schemas schemas/
 make clean
 make
-cp gtktheme.css metadata.json dist/
+cp metadata.json dist/
 cp -r schemas dist/
 cp -r src/ui dist/
 cp src/stylesheet.css dist/
